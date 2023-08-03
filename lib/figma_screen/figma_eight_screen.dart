673e571d9d42a78284fef5c9874f79039dfc7de3
dhruyan baldaniya <baldaniyadhruyan3@gmail.com>
2023-08-03 18:52:32 +0530

    3/8/23 changes

--- lib/figma_screen/figma_eight_screen.dart
+++ lib/figma_screen/figma_eight_screen.dart
@@ -92,98 +92,109 @@ class _FigmaEightScreenState extends State<FigmaEightScreen> {
     return Scaffold(
       body: ListView(
         scrollDirection: Axis.vertical,
+        padding: const EdgeInsets.only(top: 10),
         children: [
-          Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 41),
-                child: Container(
-                  height: 72,
-                  width: 400,
-                  color: const Color(0xFFF4F3FE),
-                  child: Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 20),
-                        child: Image.asset(
-                          "assets/images/9 th 1 st.png",
-                          height: 44,
-                          width: 44,
-                        ),
-                      ),
-                      SizedBox(width: 10),
-                      Text(
-                        "Evolve",
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                          color: Color(0xFF101828),
-                        ),
-                      ),
-                    ],
+          Container(
+            height: 72,
+            width: 400,
+            color: const Color(0xFFF4F3FE),
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: Image.asset(
+                    "assets/images/9 th 1 st.png",
+                    height: 44,
+                    width: 44,
                   ),
                 ),
-              ),
-              const Padding(
-                padding: EdgeInsets.only(top: 10),
-                child: Text(
-                  "Leadership Team",
+                const SizedBox(width: 10),
+                const Text(
+                  "Evolve",
                   style: TextStyle(
-                    fontSize: 28,
-                    fontWeight: FontWeight.w900,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF101828),
                   ),
                 ),
+              ],
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(top: 10),
+            child: Text(
+              "Leadership Team",
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.w900,
               ),
-              const Padding(
-                padding: EdgeInsets.only(top: 15),
-                child: Text(
-                  "   Meet the leadership team driving our\n "
-                  "mission to create a world where people\n "
-                  "  aur supported to do the bestwork of\n"
-                  "their lives. We're innovators, behavioral\n"
-                  "         scientists, talent management\n"
-                  "     strategists, and consumer product\n"
-                  "   experts that aim to make everlasting\n"
-                  "change for our members, Coaches, and \n"
-                  "                            partners.",
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w400,
-                    fontFamily: "Inter.thim",
-                  ),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(top: 15),
+            child: Text(
+              "   Meet the leadership team driving our\n "
+              "mission to create a world where people\n "
+              "  aur supported to do the bestwork of\n"
+              "their lives. We're innovators, behavioral\n"
+              "         scientists, talent management\n"
+              "     strategists, and consumer product\n"
+              "   experts that aim to make everlasting\n"
+              "change for our members, Coaches, and \n"
+              "                            partners.",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w400,
+                fontFamily: "Inter.thim",
+              ),
+            ),
+          ),
+          ListView.builder(
+            itemCount: 9,
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            scrollDirection: Axis.vertical,
+            itemBuilder: (context, index) => Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: Container(
+                height: 550,
+                width: 343,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: const Color(0xFFF3F4FE),
                 ),
-              ),
-              ListView.builder(
-                itemCount: 9,
-                shrinkWrap: true,
-                scrollDirection: Axis.vertical,
-                itemBuilder: (context, index) => Padding(
-                  padding: const EdgeInsets.only(top: 20),
-                  child: Container(
-                    height: 550,
-                    width: 343,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: const Color(0xFFF3F4FE),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(30),
+                      child: Image.asset(
+                        colortextList[index]["image"],
+                        width: 311,
+                        height: 272,
+                      ),
                     ),
-                    child: Column(
+                    Padding(
+                      padding: const EdgeInsets.only(right: 210),
+                      child: Text(
+                        colortextList[index]["text"],
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                          color: Color(0xFF101828),
+                          fontFamily: "Inter",
+                        ),
+                      ),
+                    ),
+                    Column(
                       children: [
                         Padding(
-                          padding: const EdgeInsets.all(30),
-                          child: Image.asset(
-                            colortextList[index]["image"],
-                            width: 311,
-                            height: 272,
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(right: 210),
+                          padding: const EdgeInsets.only(right: 160, top: 15),
                           child: Text(
-                            colortextList[index]["text"],
+                            colortextList[index]["title"],
                             style: const TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.w600,
-                              color: Color(0xFF101828),
+                              fontSize: 18,
+                              fontWeight: FontWeight.w400,
+                              color: Color(0xFF2B11EB),
                               fontFamily: "Inter",
                             ),
                           ),
@@ -191,47 +202,24 @@ class _FigmaEightScreenState extends State<FigmaEightScreen> {
                         Column(
                           children: [
                             Padding(
-                              padding: const EdgeInsets.only(right: 160, top: 15),
+                              padding: const EdgeInsets.only(top: 15, right: 60),
                               child: Text(
-                                colortextList[index]["title"],
+                                colortextList[index]["detail"],
                                 style: const TextStyle(
-                                  fontSize: 18,
+                                  fontSize: 16,
                                   fontWeight: FontWeight.w400,
-                                  color: Color(0xFF2B11EB),
-                                  fontFamily: "Inter",
+                                  color: Color(0xFF667085),
                                 ),
                               ),
                             ),
-                            Column(
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.only(top: 15, right: 60),
-                                  child: Text(
-                                    colortextList[index]["detail"],
-                                    style: const TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.w400,
-                                      color: Color(0xFF667085),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
                           ],
-                        )
+                        ),
                       ],
-                    ),
-                  ),
+                    )
+                  ],
                 ),
               ),
-              // Container(
-              //   height: 588,
-              //   width: 375,
-              //   decoration: const BoxDecoration(
-              //     color: Color(0xFFFFFFFF),
-              //   ),
-              // ),
-            ],
+            ),
           ),
         ],
       ),
