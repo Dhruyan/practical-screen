a9caa4dab0513d191090eec6353e13ec4b73c34a
dhruyan baldaniya <baldaniyadhruyan3@gmail.com>
2023-08-25 18:58:36 +0530

    changes

--- lib/comman widgets/foodstyle.dart
+++ lib/comman widgets/foodstyle.dart
@@ -1,1 +1,12 @@
+import 'package:flutter/material.dart';
 
+class FoodStyle extends StatelessWidget {
+  const FoodStyle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(),
+    );
+  }
+}
---
+++ lib/figma_screen/nine_sereen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class NineScreen extends StatefulWidget {
+  const NineScreen({super.key});
+
+  @override
+  State<NineScreen> createState() => _NineScreenState();
+}
+
+class _NineScreenState extends State<NineScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [
+          Image
+        ],
+      ),
+    );
+  }
+}
