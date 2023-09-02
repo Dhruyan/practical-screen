80d46a30bbe2253b4d3f94140ecaffc950189c4f
dhruyan baldaniya <baldaniyadhruyan3@gmail.com>
2023-09-02 19:09:54 +0530

    changes

--- lib/figma_screen/nine_sereen.dart
+++ lib/figma_screen/nine_sereen.dart
@@ -15,16 +15,39 @@ class _NineScreenState extends State<NineScreen> {
         children: [
           Stack(
             children: [
-              Container(
-                height: 32,
-                width: 32,
-                decoration: BoxDecoration(
-                  color: Colors.black87,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
               Image.asset(
                 "assets/images/9 th 1.png",
+              ),
+              Row(
+                children: [
+                  SizedBox(height: 60),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Container(
+                        height: 32,
+                        width: 32,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Icon(
+                            Icons.arrow_back_ios_new_sharp,
+                        ),
+                  ),
+                  ),
+                  SizedBox(height: 60),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 310),
+                    child: Container(
+                      height: 32,
+                      width: 32,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
